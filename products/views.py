931a29e57dbbdd302c564a46e0864fc532fc27ae
ThomasMSmit <Thoom12@hotmail.com>
2021-08-09 14:50:37 +0000

    add product details functionality

--- products/views.py
+++ products/views.py
@@ -12,4 +12,16 @@ def all_products(request):
         'products': products,
     }
 
-    return render(request, 'products/products.html', context)
+     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
